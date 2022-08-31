ac22c217a16d96894dd6e2ceedd139c6eaa7d864
VakotUA <vakotua.g@gmail.com>
2022-08-31 13:56:34 +0300

    styles fix

--- src/components/Portfolio/index.tsx
+++ src/components/Portfolio/index.tsx
@@ -45,36 +45,38 @@ const Portfolio: React.FC = () => {
   return (
     <motion.section id="portfolio" className={style.Portfolio}>
       <Container className={style.Content}>
-        <Text.H3
-          initial="hidden"
-          whileInView="visible"
-          viewport={{ amount: 0.2, once: true }}
-          custom={1}
-          variants={TextAnimations.leftToRight}
-        >
-          OUR PORTFOLIO
-        </Text.H3>
-        <motion.div
-          className={style.Tabs}
-          initial="hidden"
-          whileInView="visible"
-          viewport={{ amount: 0.2, once: true }}
-        >
-          <ul>
-            <motion.li custom={1} variants={TextAnimations.topToBottom}>
-              <Text.P className={style.Active}>ALL</Text.P>
-            </motion.li>
-            <motion.li custom={2} variants={TextAnimations.topToBottom}>
-              <Text.P>LANDING</Text.P>
-            </motion.li>
-            <motion.li custom={3} variants={TextAnimations.topToBottom}>
-              <Text.P>E-COMMERCE</Text.P>
-            </motion.li>
-            <motion.li custom={4} variants={TextAnimations.topToBottom}>
-              <Text.P>DESIGN</Text.P>
-            </motion.li>
-          </ul>
-        </motion.div>
+        <div>
+          <Text.H3
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ amount: 0.2, once: true }}
+            custom={1}
+            variants={TextAnimations.leftToRight}
+          >
+            OUR PORTFOLIO
+          </Text.H3>
+          <motion.div
+            className={style.Tabs}
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ amount: 0.2, once: true }}
+          >
+            <ul>
+              <motion.li custom={1} variants={TextAnimations.topToBottom}>
+                <Text.P className={style.Active}>ALL</Text.P>
+              </motion.li>
+              <motion.li custom={2} variants={TextAnimations.topToBottom}>
+                <Text.P>LANDING</Text.P>
+              </motion.li>
+              <motion.li custom={3} variants={TextAnimations.topToBottom}>
+                <Text.P>E-COMMERCE</Text.P>
+              </motion.li>
+              <motion.li custom={4} variants={TextAnimations.topToBottom}>
+                <Text.P>DESIGN</Text.P>
+              </motion.li>
+            </ul>
+          </motion.div>
+        </div>
       </Container>
 
       <div className={style.TabContent}>
